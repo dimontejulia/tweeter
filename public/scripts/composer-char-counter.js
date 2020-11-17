bf7b100b39c6d7b3eff0415d7d426136813b7400
Julia Di Monte <dimontejulia@gmail.com>
2020-11-17 17:34:25 +0000

    removing console logs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   $("#tweet-text").on("input", function () {
     const charCount = $(this).val().length;
     if (140 - charCount < 0) {
-      console.log("hey");
       $(".counter").addClass("negativeNum");
     } else if (140 - charCount >= 0) {
       $(".counter").removeClass("negativeNum");
