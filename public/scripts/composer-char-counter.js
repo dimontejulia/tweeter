002ae7874d5b465624e146f2e82aee8ae833ea3c
Julia Di Monte <dimontejulia@gmail.com>
2020-11-17 19:57:57 +0000

    updated counter to use the DOM

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
   $("#tweet-text").on("input", function () {
     const charCount = $(this).val().length;
+
+    const counter = $(this).siblings(".form-footer").children(".counter");
+    console.log("counter", counter);
     if (140 - charCount < 0) {
-      $(".counter").addClass("negativeNum");
+      counter.addClass("negativeNum");
     } else if (140 - charCount >= 0) {
-      $(".counter").removeClass("negativeNum");
+      counter.removeClass("negativeNum");
     }
-    $(".counter").text(140 - charCount);
+    counter.text(140 - charCount);
   });
 });
