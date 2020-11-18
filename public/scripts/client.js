a36294d39f4bbb0e9008970e1b805359d846bdb6
Julia Di Monte <dimontejulia@gmail.com>
2020-11-18 21:00:21 +0000

    adding ajax for new tweet requests

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -32,31 +32,29 @@ const renderTweets = function (arrOfTweets) {
   }
 };
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text:
-        "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+$(document).ready(() => {
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+    }).then((tweets) => {
+      renderTweets(tweets);
+      console.log("AJAX request successful");
+    });
+  };
 
-$(document).ready(() => renderTweets(data));
+  loadTweets();
+
+  $("form").on("submit", (event) => {
+    event.preventDefault();
+
+    const tweetBody = $("form textarea").val();
+    const serializedForm = $("form").serialize();
+    //ajax request
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: serializedForm,
+    }).then(() => loadTweets());
+  });
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     const charCount = $(this).val().length;
 
     const counter = $(this).siblings(".form-footer").children(".counter");
-    console.log("counter", counter);
     if (140 - charCount < 0) {
       counter.addClass("negativeNum");
     } else if (140 - charCount >= 0) {
