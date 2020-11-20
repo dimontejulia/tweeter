bde7f52d8f93ab622db12a28fb64b949102fe6b5
Julia Di Monte <dimontejulia@gmail.com>
2020-11-20 00:13:02 +0000

    final comments and more helper functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,58 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-//takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
-const createTweetElement = function (tweetObj) {
-  const content = $("<div>").text(tweetObj.content.text).html();
-  const tweet = `<article>
-  <header>
-    <img src="${tweetObj.user.avatars}" />
-    <p>${tweetObj.user.name}</p>
-    <a class="handle" href="#">${tweetObj.user.handle}</a>
-  </header>
-  <p class="tweet-body">
-  ${content}
-  </p>
-  <footer>
-    <p>10 days ago</p>
-    <p class="symbols">&#9873 &#10227; &#10084;</p>
-  </footer>
-</article>`;
-
-  return tweet;
-};
-
-//taking in an array of tweet objects and then appending each one to the #tweets-container
-const renderTweets = function (arrOfTweets) {
-  // loops through tweets
-  for (let tweet of arrOfTweets) {
-    $("#tweets-container").prepend(createTweetElement(tweet));
-  }
-};
-
-//validates that tweet is valid, otherwise display error message
-const validateTweet = function (tweetBody) {
-  if (tweetBody.length <= 140 && tweetBody !== "" && tweetBody !== null) {
-    $("#error").hide();
-    return true;
-  } else {
-    $("#error").show();
-    return false;
-  }
-};
-
+//ensures whole document is loaded
 $(document).ready(() => {
+  //hides items that are toggled by events
   $("#error").hide();
   $(".new-tweet").hide();
   $("#topButton").hide();
 
+  //helper functions for revealing more features
   loadTweets();
   toggleTweet();
   scrollButton();
 
+  //creates and adds new tweet to the list
   $("form").on("submit", (event) => {
     event.preventDefault();
-
     const tweetBody = validateTweet($("form textarea").val());
     if (tweetBody) {
       const serializedForm = $("form").serialize();
--- public/scripts/helper.js
+++ public/scripts/helper.js
@@ -1,5 +1,46 @@
 //helper functions for modularity
 
+//takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
+const createTweetElement = function (tweetObj) {
+  const content = $("<div>").text(tweetObj.content.text).html();
+  const tweet = `<article>
+  <header>
+    <img src="${tweetObj.user.avatars}" />
+    <p>${tweetObj.user.name}</p>
+    <a class="handle" href="#">${tweetObj.user.handle}</a>
+  </header>
+  <p class="tweet-body">
+  ${content}
+  </p>
+  <footer>
+    <p>10 days ago</p>
+    <p class="symbols">&#9873 &#10227; &#10084;</p>
+  </footer>
+</article>`;
+
+  return tweet;
+};
+
+//taking in an array of tweet objects and then appending each one to the #tweets-container
+const renderTweets = function (arrOfTweets) {
+  // loops through tweets
+  for (let tweet of arrOfTweets) {
+    $("#tweets-container").prepend(createTweetElement(tweet));
+  }
+};
+
+//validates that tweet is valid, otherwise display error message
+const validateTweet = function (tweetBody) {
+  if (tweetBody.length <= 140 && tweetBody !== "" && tweetBody !== null) {
+    $("#error").hide();
+    return true;
+  } else {
+    $("#error").show();
+    return false;
+  }
+};
+
+//ajax get function to load all logged tweets
 const loadTweets = function () {
   $.ajax({
     url: "/tweets",
@@ -15,6 +56,7 @@ const toggleTweet = function () {
   });
 };
 
+//reveals arrow to link to the top of the page when user scrolls
 const scrollButton = function () {
   $(window).scroll(function () {
     if ($(".container").scrollTop() > 500) {
